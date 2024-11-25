94658f218e010ade04c2cc898d6227815a5cd5bd
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-11-25 21:40:10 +0100

    -

--- src/crew/models.py
+++ src/crew/models.py
@@ -1,15 +1,15 @@
 from flask                    import current_app
 from flask_security.models    import fsqla_v3                                               as fsqla
-from sqlalchemy.orm           import DeclarativeBase,MappedAsDataclass,Mapped,mapped_column
+from sqlalchemy               import ForeignKey
+from sqlalchemy.orm           import DeclarativeBase,MappedAsDataclass,Mapped,mapped_column,relationship
 from flask_security           import SQLAlchemyUserDatastore
 from flask_admin.contrib.sqla import ModelView
 
-class CrewMember(User):
+class CrewMember(current_app.database.Model):
     __tablename__ = "crew_members"
-    nickname:   Mapped[str] = mapped_column(primary_key=True)
-    rank:       Mapped[str]
-    first_name: Mapped[str]
-    last_name:  Mapped[str]
+    user:     Mapped[str] = mapped_column(ForeignKey("user.username"))
+    nickname: Mapped[str] = mapped_column(primary_key=True)
+    rank:     Mapped[str] = mapped_column(nullable=False)
 
 current_app.database.create_all()
 
