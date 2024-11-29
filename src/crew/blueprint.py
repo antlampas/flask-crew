ce3a5a3c9eefe714c87437554e2fc127d41edb95
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-11-29 12:04:17 +0100

    -

--- src/crew/blueprint.py
+++ src/crew/blueprint.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 crew_blueprint = Blueprint('crew',__name__,url_prefix='/crew',template_folder='templates',static_folder='static')
 
-current_menu.submenu(".articles")
+current_menu.submenu(".crew")
