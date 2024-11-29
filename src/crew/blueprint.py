546d753623f14539e334ec49d22a831de5ca24cd
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-11-29 12:27:27 +0100

    -

--- src/crew/blueprint.py
+++ src/crew/blueprint.py
@@ -3,4 +3,4 @@ from flask_menu import current_menu
 
 crew_blueprint = Blueprint('crew',__name__,url_prefix='/crew',template_folder='templates',static_folder='static')
 
-current_menu.submenu(".crew").register(text='Crew',external_url=crew_blueprint.url_prefix,visible_when=None)
+current_menu.submenu(".crew").register(text='Crew',external_url=crew_blueprint.url_prefix).hide()
